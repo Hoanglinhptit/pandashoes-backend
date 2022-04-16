8ec6800a18cfe8eeddd18f719752c03eaa9b69db
truong <truongsoul3@gmail.com>
2022-04-16 21:06:09 +0700

    update

--- api/services/CategoryServices.js
+++ api/services/CategoryServices.js
@@ -23,8 +23,8 @@ const getCategory = (req, res) => {
             //search paginate
             Category.find({ name: { $regex: keySearch, $options: 'i' } }).skip(utilsPagination.getOffset(pageIndex, limit)).limit(parseInt(limit)).exec((err, data) => {
                 if (err) return res.json(response.error(err))
-                let pagination = utilsPagination.pagination(data, limit, pageIndex, Category, { name: { $regex: keySearch, $option: 'i' } })
-                res.json(response.success(pagination))
+                utilsPagination.pagination(data, limit, pageIndex, Category, res, { name: { $regex: keySearch, $option: 'i' } })
+
             })
         }
     }
@@ -33,8 +33,8 @@ const getCategory = (req, res) => {
         if (pageIndex !== "undefined") {
             Category.find({}).skip(utilsPagination.getOffset(pageIndex, limit)).limit(parseInt(limit)).exec((err, data) => {
                 if (err) return res.json(response.error(err))
-                let pagination = utilsPagination.pagination(data, limit, pageIndex, Category)
-                res.json(response.success(pagination))
+                utilsPagination.pagination(data, limit, pageIndex, Category, res)
+
             })
         } else if (pageIndex === "undefined") {
             Category.find({}).limit(parseInt(limit)).exec((error, data) => {
@@ -48,29 +48,35 @@ const getCategory = (req, res) => {
 }
 
 const createCategory = async (req, res) => {
-    const { pageIndex, limit } = req.query
-    const { category } = req.body
-    console.log(category);
-    const exist = await Category.find({ name: { $regex: category, $options: 'i' } }, { __v: 0 })
-    console.log(exist.length);
+    const { limit, keySearch } = req.query
+    const { name } = req.body
+    // console.log(category);
+    const exist = await Category.find({ name: { $regex: name, $options: 'i' } }, { __v: 0 })
+    // console.log(exist.length);
     if (exist.length >= 1) { return res.json(response.error({ message: "name exist" })) }
-    const newCategory = new Category({ name: category })
+    const newCategory = new Category({ name: name })
     newCategory.save((err, data) => {
         if (err) return res.json(response.error(err))
+        Category.countDocuments({ name: { $regex: keySearch, $options: 'i' } }, (err1, data1) => {
+            if (err1) return res.json(response.error(err))
 
-        let dataResult = {
-            data,
-            pageIndex: Math.ceil((Category.countDocuments()) / parseInt(limit)),
-            limit
-        }
-        res.json(response.success(dataResult))
+            let dataResult = {
+                data,
+                pageIndex: Math.ceil(data1 / parseInt(limit)),
+                limit, keySearch
+            }
+            console.log(dataResult);
+            res.json(response.success(dataResult))
+        })
+
+
     })
 
 }
 const updatecategory = (req, res) => {
     const id = req.query.id
-    const { nameCategory } = req.body
-    Category.findByIdAndUpdate({ _id: id }, { name: nameCategory }).exec((err, data) => {
+    const { name } = req.body
+    Category.findByIdAndUpdate({ _id: id }, { name: name }).exec((err, data) => {
         if (err) return res.json(response.error(err))
         res.json(response.success(data))
     })
--- api/utils/pagination.js
+++ api/utils/pagination.js
@@ -1,3 +1,5 @@
+const res = require("express/lib/response")
+const response = require("../common/response")
 function getOffset(page, limit) {
     let pageIndex = parseInt(page) < 1 ? 1 : parseInt(page)
     let limitElement = parseInt(limit) < 1 ? 1 : parseInt(limit)
@@ -7,22 +9,23 @@ function countTotalPage(totalRecord, limit) {
     return Math.ceil(totalRecord / parseInt(limit))
 }
 
-function pagination(data, limit, pageIndex, model, filter) {
+function pagination(data, limit, pageIndex, model, res, filter) {
     let totalRecord
-    if (model.length == 0) {
-        totalRecord = 1
-    } else {
-        totalRecord = model.countDocuments(filter || {})
-    }
 
-    let totalPage = countTotalPage(totalRecord, limit)
-    return {
-        limit: parseInt(limit) < 1 ? 1 : parseInt(limit),
-        data,
-        pageIndex: parseInt(pageIndex) < 1 ? 1 : parseInt(pageIndex),
-        totalPage
-    }
+    model.countDocuments(filter || {}).exec(async (err, dataCount) => {
+        if (err) return err
+        totalRecord = dataCount
+        let totalPage = countTotalPage(totalRecord, limit)
+        res.json(response.success({
+            limit: parseInt(limit) < 1 ? 1 : parseInt(limit),
+            data,
+            pageIndex: parseInt(pageIndex) < 1 ? 1 : parseInt(pageIndex),
+            totalPage
+        }))
+
+    })
 }
+
 module.exports = {
     getOffset, pagination
 }
