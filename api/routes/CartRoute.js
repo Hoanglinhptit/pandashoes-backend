0d16cd93f7b631725e7445ed2efd6174f5d9f1f8
truong <truongsoul3@gmail.com>
2022-05-08 09:12:47 +0700

    update

---
+++ api/routes/CartRoute.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+    const cartServices = require('../services/CartShoppingServices')
+    app.route('/cart')
+        // .get(categoryServices.getCategory)
+        // .post(categoryServices.createCategory)
+        // .put(categoryServices.updatecategory)
+        // .delete(categoryServices.deleteCategory)
+
+}
