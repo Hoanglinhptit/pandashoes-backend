e920694bf3c590dc61b2ba21ae2ab5ee18a84fa7
Hoang Linh <linh.lh@tinasoft.vn>
2023-05-22 09:25:00 +0700

    os cpus optimize

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require('http');
+const http = require("http");
+const https = require("https");
 const io = require("socket.io");
 const AWS = require("aws-sdk");
 const bodyParser = require("body-parser");
-const { handleConnection }  = require('./api/services/ChatServices');
+// require("dotenv").config();
+const os = require("os");
+const { handleConnection } = require("./api/services/ChatServices");
 mongoose.Promise = global.Promise;
 
 const routes = require("./api/routes");
@@ -19,18 +22,17 @@ AWS.config.update({
   secretAccessKey: "zUMH2PbOGocjsV0LT5i/3/BL/SNDMVMbchUMt08M",
 });
 
-
-
+process.env.UV_THREAD_POOL_SIZE = os.cpus().length;
 app.use(express.static(path.join(__dirname, "public/image")));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tpm/" }));
 const server = http.createServer(app);
-const socket = io(server)
-socket.on('connection', handleConnection);
+const socket = io(server);
+socket.on("connection", handleConnection);
 routes(app);
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 mongoose
   .connect("mongodb://localhost:27017/panda-shoes", { useNewUrlParser: true })
   .then(() => {
