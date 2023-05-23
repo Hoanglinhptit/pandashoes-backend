99e1f735ed753a568b5dac8fdd6d129b9360195d
Hoang Linh <linh.lh@tinasoft.vn>
2023-05-24 01:04:12 +0700

    ssl key

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
-const https = require("https");
+const https = require('https');
 const io = require("socket.io");
+const fs = require("fs");
 const AWS = require("aws-sdk");
 const bodyParser = require("body-parser");
 // require("dotenv").config();
@@ -21,17 +22,21 @@ AWS.config.update({
   accessKeyId: "AKIAQR5LWODMYC2RUX5D",
   secretAccessKey: "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",
 });
-
+const options = {
+  key: fs.readFileSync("./certificates_ssl/domain.key"),
+  cert: fs.readFileSync("./certificates_ssl/domain.crt"),
+};
+const server = http.createServer(options, app);     
+const socket = io(server);
+socket.on("connection", handleConnection);
 process.env.UV_THREAD_POOL_SIZE = os.cpus().length;
 app.use(express.static(path.join(__dirname, "public/image")));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tpm/" }));
-const server = http.createServer(app);
-const socket = io(server);
-socket.on("connection", handleConnection);
 routes(app);
+
 mongoose.set("strictQuery", false);
 mongoose
   .connect("mongodb://localhost:27017/panda-shoes", { useNewUrlParser: true })
